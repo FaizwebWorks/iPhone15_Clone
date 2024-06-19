41e0c269c6dbf7468d7e16e5875f960b6b33dff0
FaizwebWorks <webWorksbyfaiz@gmail.com>
2024-06-19 16:52:50 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-t5",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "jsm-t5",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "jsm-t5",
-    project: "iphone-15"
-  })],
+  plugins: [
+    react(),
+    {
+      org: "jsm-t5",
+      project: "javascript-react",
+    },
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
